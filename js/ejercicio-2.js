eb33e226d378b84836ab27ad073a2e8e169246a4
melisa casas <lucioagustinlazarte@gmail.com>
2023-03-15 19:32:53 -0300

    ejercicio2terminado

--- js/ejercicio-2.js
+++ js/ejercicio-2.js
@@ -1,5 +1,32 @@
+
 let ciudades = [];
 
-for (let i = 0; i <3; i++) {
-    ciudades.push(prompt(`Introduce el nombre ${ciudades}`));
+while (true) {
+  var ciudad = prompt('Ingresa una ciudad (o presiona Cancelar para terminar) ');
+  if (ciudad === null) {
+    break;
+  }
+  ciudades.push(ciudad);
 }
+
+
+document.write(ciudades);
+
+// Mostramos la longitud del arreglo
+document.write("Longitud del arreglo: " + ciudades.length);
+
+document.write("Ciudades en las posiciones primera, tercera y última:<br>");
+document.write(ciudades[0] + "<br>");
+document.write(ciudades[2] + "<br>");
+document.write(ciudades[ciudades.length-1] + "<br>");
+
+
+ciudades.push("París");
+document.write(ciudades);
+
+
+console.log("El elemento en la segunda posición es: " + ciudades[1]);
+
+
+ciudades.splice(1, 1, "Barcelona");
+document.write(ciudades);
